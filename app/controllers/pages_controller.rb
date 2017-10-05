51acf3b61c96cc6cbafed79c0babacec65e2a89a
DeadFred <f.cliff1@hotmail.com>
2017-10-05 15:51:55 +1000

    rolify working, users able to filter through other users with roles, user images dependant on user role

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+
   def profile
+    @user = current_user
+    if params[:role]
+      @users = User.with_any_role(params[:role].to_sym)
+    else
+      @users = User.all
+    end
   end
+
 end
